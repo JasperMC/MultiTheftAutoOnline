4a66170b87266d784b23afadb401eb3b17360ff2
Jasper C. <37154053+JasperMC@users.noreply.github.com>
2025-10-26 19:34:43 +0100

    Add gamemode freemode

---
+++ server/mods/deathmatch/resources/[custom]/[gamemode]/freemode/server/freemode_events.lua
@@ -0,0 +1,58 @@
+-- On Resource Start
+addEventHandler("onResourceStart", resourceRoot,
+    function()
+    
+    end
+)
+
+-- On Resource Stop
+addEventHandler("onResourceStop", resourceRoot,
+    function()
+    
+    end
+)
+
+
+-- On Gamemode Start
+addEventHandler("onGamemodeMapStart", resourceRoot, 
+    function()
+    
+    end
+)
+
+addEventHandler("onGamemodeMapStop", resourceRoot,
+    function()
+    
+    end
+)
+
+
+
+
+-- On Player join
+addEventHandler("onPlayerJoin", root, 
+    function()
+    
+    end
+)
+
+-- On Player Login
+addEventHandler("onPlayerLogin", root,
+    function()
+    
+    end
+)
+
+-- On Player Spawn
+addEventHandler("onPlayerSpawn", root,
+    function()
+    
+    end
+)
+
+addEventHandler("onPlayerWasted", root,
+    function()
+    
+    end
+)
+
