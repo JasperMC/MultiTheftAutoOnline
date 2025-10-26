42f9df4f39ab852894728b807da7a27aaea875c7
Jasper C. <37154053+JasperMC@users.noreply.github.com>
2025-10-26 12:03:21 -0700

    Update spawnpoint_functions_s.lua

--- server/mods/deathmatch/resources/[custom]/[gamemode]/freemode/server/spawnpoint_functions_s.lua
+++ server/mods/deathmatch/resources/[custom]/[gamemode]/freemode/server/spawnpoint_functions_s.lua
@@ -3,14 +3,29 @@
 ]]
 
 function getSpawnpointType( spawnpoint )
-
+  return getElementData( spawnpoint, "type")
 end
 
 function getSpawnpointsByType( spawnpoints, type )
-
+  spawnpoints_of_type = {}
+  for i, spawnpoint in ipairs(spawnpoints) do
+    if getSpawnpointType(spawnpoint) == type then
+      spawnpoints_of_type.append(spawnpoint)
+    end
+  end
+  return spawnpoints_of_type or false
 end
 
 function getClosestSpawnpoint( element, spawnpoints )
-
+ local el_x, el_y, el_z = getElementPosition( element )
+ local closest_spawnpoint = false
+ local closest_distance = 9999
+ for i, spawnpoint in ipairs(spawnpoints) do
+   local s_x, s_y, s_z = getElementPosition( spawnpoint )
+   local distance = getDistanceBetweenPoints3D( el_x, el_y, el_z, s_x, s_y, s_z )
+   if distance < closest_distance then
+     closest_spawnpoint = spawnpoint
+     closest_distance = distance
+   return closest_spawnpoint, closest_distance
 end
 
