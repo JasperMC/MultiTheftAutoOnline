4823b6c0ec0eb0c5f82cceb44771d53598f1534a
Jasper C. <37154053+JasperMC@users.noreply.github.com>
2025-10-27 12:57:59 -0700

    Create business_server.lua

---
+++ server/mods/deathmatch/resources/[custom]/[gameplay]/[world]/business/server/business_server.lua
@@ -0,0 +1,36 @@
+
+
+function createBusiness( business )
+
+end
+
+function setBusinessState( state )
+
+end
+
+function vendBusinessProduct( business, product, sell_to )
+
+end
+
+function addBusinessOccupant( business, occupant )
+
+end
+
+function removeBusinessOccupant( business, occupant )
+
+end
+
+function spawnBusinessVehicle( business, vehicle )
+
+end
+
+function addBusinessVehicle( business, vehicle )
+
+end
+
+function removeBusinessVehicle( business, vehicle )
+
+end
+
+
+
